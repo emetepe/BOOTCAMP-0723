998895fad13d6722f49c4ead442f2fdb3e190709
Emetepe <martape@gmail.com>
2023-07-05 08:02:09 +0200

    Iteration#3

--- JAVASCRIPT/neoland-basic-algorithms/app.js
+++ JAVASCRIPT/neoland-basic-algorithms/app.js
@@ -50,3 +50,37 @@ car2.finalPrice = car2.finalPrice + globalBasePrice;
 
 console.log(car1.finalPrice);
 console.log(car2.finalPrice);
+
+//Iteración #3: Operadores
+
+// 3.1 Multiplica 10 por 5 y muestra el resultado mediante console.
+let mult1 = 10;
+let mult2 = 5;
+multiplicación = mult1 * mult2;
+console.log(`${mult1} * ${mult2} = ${multiplicación}`);
+
+// 3.2 Divide 10 por 2 y muestra el resultado en un console.
+let div1 = 10;
+let div2 = 2;
+division = div1 / div2;
+console.log(`${div1} / ${div2} es ${division}`)
+
+// 3.3 Muestra mediante un console el resto de dividir 15 por 9.
+let rest1 = 10;
+let rest2 = 2;
+resto = rest1 % rest2;
+console.log(`${rest1} % ${div2} es ${rest2}`)
+
+/* 3.4 Usa el correcto operador de asignación que resultará en o = 15, 
+teniendo dos variables p = 10 y j = 5.*/
+let p = 10;
+let j = 5;
+let o = p + j;
+console.log(o)
+
+/* 3.5 Usa el correcto operador de asignación que resultará en i = 50,
+teniendo dos variables c = 10 y m = 5. */
+let c = 10;
+let m = 5;
+let i = c * m;
+console.log(`El operador es *`)
