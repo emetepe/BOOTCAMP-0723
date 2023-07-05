d7f6d31fe51c1f29f10046765124e3669de14aff
Emetepe <martape@gmail.com>
2023-07-05 21:54:57 +0200

    Iteración#Correcciones y mejoras

--- JAVASCRIPT/neoland-basic-algorithms/app.js
+++ JAVASCRIPT/neoland-basic-algorithms/app.js
@@ -1,17 +1,19 @@
+// Neoland JavaScript Basic algorithms
+
 // Iteración #1: Variables //
 
 // 1.1 Crea una variable llamada myFavoriteHero, asigna el valor Hulk a ella.
-let myFavoriteHero = "Hulk"
+let myFavoriteHero = "Hulk";
 
 // 1.2 Crea una variable llamada x, asigna el valor 50 a ella.
-let x = 50
+let x = 50;
 
 // 1.3 Crea una variable llamada 'h' con el valor 5 y otra 'y' con el valor 10. 
 let h = 5;
-let y = 10
+let y = 10;
 
 // 1.4 Crea una otra variable 'z' y asignale el valor de 'h' + 'y'.
-let z = h + y
+let z = h + y;
 
 // -----------------------------------
 
@@ -23,12 +25,12 @@ character.age = 25;
 console.log(character.age);
 
 // 2.2 Declara 3 variables con los nombres y valores siguientes 
-	firstName = 'Jon'; 
-	lastName = 'Snow'; 
-	age = 24; 
+let firstName = 'Jon'; 
+let lastName = 'Snow'; 
+let age = 24;
 /* Muestralos por consola de esta forma: 
 	'Soy Jon Snow, tengo 24 años y me gustan los lobos.'*/
-	console.log(`Soy ${firstName} ${lastName}, tengo ${age} años y me gustan los lobos`);
+console.log(`Soy ${firstName} ${lastName}, tengo ${age} años y me gustan los lobos`);
 
 	
 /* 2.3 Dado el siguiente javascript, imprime con un console.log 
@@ -36,8 +38,7 @@ la suma del precio de ambos juguetes.*/
 const toy1 = {name: 'Buss myYear', price: 19};
 const toy2 = {name: 'Rallo mcKing', price: 29};
 
-total = toy1.price + toy2.price;
-console.log(total);
+console.log(`La suma del precio de ambos juguetes es ${toy1.price + toy2.price}`);
 
 /* 2.4 Dado el siguiente javascript, actualiza el valor de la variable globalBasePrice a 25000 
 y actualiza la propiedad finalPrice de todos los coches con el valor de su propiedad 
@@ -47,79 +48,81 @@ const car1 = {name: 'BMW m&m', basePrice: 50000, finalPrice: 60000};
 const car2 = {name: 'Chevrolet Corbina', basePrice: 70000, finalPrice: 80000};
 
 globalBasePrice = 25000;
-car1.finalPrice = car1.finalPrice + globalBasePrice;
-car2.finalPrice = car2.finalPrice + globalBasePrice;
 
-console.log(car1.finalPrice);
-console.log(car2.finalPrice);
+console.log(`El precio final del coche 1 es ${car1.finalPrice = globalBasePrice + car1.basePrice}`);
+console.log(`El precio final del coche 2 es ${car2.finalPrice = globalBasePrice + car2.basePrice}`);
 
 // -----------------------------------
 
 //Iteración #3: Operadores
 
 // 3.1 Multiplica 10 por 5 y muestra el resultado mediante console.
-let mult1 = 10;
-let mult2 = 5;
-multiplicación = mult1 * mult2;
-console.log(`${mult1} * ${mult2} = ${multiplicación}`);
+let x = 10;
+let y = 5;
+let mult = x * y;
+
+console.log(`El resultado de x por y es igual a ${mult}`);
 
 // 3.2 Divide 10 por 2 y muestra el resultado en un console.
-let div1 = 10;
-let div2 = 2;
-division = div1 / div2;
-console.log(`${div1} / ${div2} es ${division}`)
+let x = 10;
+let y = 2;
+let div = x / y;
+
+console.log(`El resultado de x entre y es igual a ${div}`);
 
 // 3.3 Muestra mediante un console el resto de dividir 15 por 9.
-let rest1 = 10;
-let rest2 = 2;
-resto = rest1 % rest2;
-console.log(`${rest1} % ${div2} es ${rest2}`)
+let x = 15;
+let y = 9;
+let resto = x % y;
+
+console.log(`El resto de x entre y es igual a ${resto}`);
 
 /* 3.4 Usa el correcto operador de asignación que resultará en o = 15, 
 teniendo dos variables p = 10 y j = 5.*/
 let p = 10;
 let j = 5;
 let o = p + j;
-console.log(o)
 
 /* 3.5 Usa el correcto operador de asignación que resultará en i = 50,
 teniendo dos variables c = 10 y m = 5. */
 let c = 10;
 let m = 5;
 let i = c * m;
-console.log(`El operador es *`)
 
 // -----------------------------------
 
 // Iteración #4: Arrays
 
 // 4.1 Consigue el valor "HULK" del array de avengers y muestralo por consola.
-const avengers1 = ["HULK", "SPIDERMAN", "BLACK PANTHER"];
-valor = avengers1[0];
-console.log(valor)
+const avengers = ["HULK", "SPIDERMAN", "BLACK PANTHER"];
+
+console.log(avengers[0]);
 
 // 4.2 Cambia el primer elemento de avengers a "IRONMAN"
-const avengers2 = ["HULK", "SPIDERMAN", "BLACK PANTHER"];
-avengers2[0] = "IRONMAN";
-console.log(avengers2)
+const avengers = ["HULK", "SPIDERMAN", "BLACK PANTHER"];
+avengers[0] = "IRONMAN";
+console.log(avengers[0]);
 
 // 4.3 console numero de elementos en el array usando la propiedad correcta de Array.
-const avengers3 = ["HULK", "SPIDERMAN", "BLACK PANTHER"];
+const avengers = ["HULK", "SPIDERMAN", "BLACK PANTHER"];
+let numElements = avengers.length;
+console.log(numElements);
 
-items = avengers3.length;
-console.log(items)
-
-// 4.4 Añade 2 elementos al array: "Morty" y "Summer". 
-// Muestra en consola el último personaje del array
-const rickAndMortyCharacters1 = ["Rick", "Beth", "Jerry"];
+/* 4.4 Añade 2 elementos al array: "Morty" y "Summer". 
+  Muestra en consola el último personaje del array */
+const rickAndMortyCharacters = ["Rick", "Beth", "Jerry"];
+rickAndMortyCharacters.push("Morty", "Summer");
+numElements = rickAndMortyCharacters.length - 1;
+console.log(rickAndMortyCharacters[numElements]);
 
 // 4.5 Elimina el último elemento del array y muestra el primero y el último por consola.
-const rickAndMortyCharacters2 = ["Rick", "Beth", "Jerry", "Morty", "Summer", "Lapiz Lopez"];
-rickAndMortyCharacters2.pop();
-console.log(rickAndMortyCharacters2[0]);
-console.log(rickAndMortyCharacters2[rickAndMortyCharacters2.length-1]);
+const rickAndMortyCharacters = ["Rick", "Beth", "Jerry"];
+rickAndMortyCharacters.pop();
+numElements = rickAndMortyCharacters.length - 1;
+console.log(rickAndMortyCharacters[0]);
+console.log(rickAndMortyCharacters[numElements])
 
 // 4.6 Elimina el segundo elemento del array y muestra el array por consola.
-const rickAndMortyCharacters3 = ["Rick", "Beth", "Jerry", "Morty", "Summer", "Lapiz Lopez"];
-rickAndMortyCharacters3.splice(1,1);
-console.log(rickAndMortyCharacters3)
+const rickAndMortyCharacters = ["Rick", "Beth", "Jerry"];
+rickAndMortyCharacters.splice(1,1);
+console.log(rickAndMortyCharacters);
