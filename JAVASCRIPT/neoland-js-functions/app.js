91aede229ba96ead5badc5c4f608b345f354ebc2
Emetepe <martape@gmail.com>
2023-07-07 14:58:15 +0200

    Iteración#2_Functions

--- JAVASCRIPT/neoland-js-functions/app.js
+++ JAVASCRIPT/neoland-js-functions/app.js
@@ -1,18 +1,57 @@
 // Ejercicios JS functions
 
+// -----------------------------------------------------------------
 // **Iteración #1: Buscar el máximo**
+// -----------------------------------------------------------------
 
 // 1.1. Completa la función que tomando dos números como argumento devuelva el más alto.
-function sum(numberOne , numberTwo) {
-    if (numberOne > numberTwo) {
-        console.log(`El número ${numberOne} es el más alto`);
-    } else if (numberTwo > numberOne) {
-            console.log(`El número ${numberTwo} es el más alto`);
-        } else {console.log("Los dos números son iguales")
-    }
-  }
 
-  let number1 = 3;
-  let number2 = 2;
+// ------------- Con function "normal" ----------------
 
-  sum(number1,number2);
+function max(numberOne , numberTwo) {
+   if (numberOne > numberTwo) {
+       console.log(`El número ${numberOne} es el más alto`);
+   } else if (numberTwo > numberOne) {
+          console.log(`El número ${numberTwo} es el más alto`);
+       } else {console.log("Los dos números son iguales")
+   }
+ }
+
+ let number1 = 3;
+ let number2 = 2;
+
+ max(number1,number2); 
+
+
+// ------------- Con arrow function ----------------
+
+const max = (numberOne, numberTwo) => {
+   if (numberOne > numberTwo) {
+       console.log(`El número ${numberOne} es el más alto`);
+   } else if (numberTwo > numberOne) {
+           console.log(`El número ${numberTwo} es el más alto`);
+       } else {console.log("Los dos números son iguales")
+   }
+}
+let number1 = 3;
+let number2 = 2;
+
+max(number1,number2);
+
+
+// -----------------------------------------------------------------
+// **Iteración #2: Buscar la palabra más larga**
+// -----------------------------------------------------------------
+
+/* Completa la función que tomando un array de strings como argumento devuelva el más largo, 
+en caso de que dos strings tenga la misma longitud deberá devolver el primero.
+Puedes usar este array para probar tu función: */
+
+const avengers = ['Hulk', 'Thor', 'IronMan', 'Captain A.', 'Spiderman', 'Captain M.'];
+
+// ---- Con arrow function y ternario ----------------
+
+let avenger = avengers.reduce((acc, val) => acc.length > val.length ? acc : val, '');
+
+console.log(avenger);
+
