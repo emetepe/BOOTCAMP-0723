25adcf6350c33829483895315bb34b21c3d991f5
Emetepe <martape@gmail.com>
2023-07-09 12:27:22 +0200

    Iteracion#3_ES6

--- JAVASCRIPT/neoland-js-es6/app.js
+++ JAVASCRIPT/neoland-js-es6/app.js
@@ -8,14 +8,21 @@
 que por defecto el valor de a = 10 y de b = 5. Haz que la función muestre 
 por consola la suma de los dos parametros. */
 
-// 1.1. Ejecuta esta función sin pasar ningún parametro
+let a = 10;
+let b = 5;
 
+const sum = (n1, n2) => {
+    return n1 + n2
+}
+
+// 1.1. Ejecuta esta función sin pasar ningún parametro
+console.log(sum())
 
 // 1.2 Ejecuta esta función pasando un solo parametro
-
+console.log(sum(a))
 
 // 1.3 Ejecuta esta función pasando dos parametros
-
+console.log(sum(a,b))
 
 // -----------------------------------------------------------------
 // ** Iteración #2: Destructuring **
@@ -27,11 +34,22 @@ no hace falta hacer destructuring del array, solo del objeto. */
 
 const game = {title: 'The last us 2', gender: ['action', 'zombie', 'survival'], year: 2020}
 
+let {title, gender, year} = game;
+
+console.log(title);
+console.log(gender);
+console.log(year);
+
 /* 2.2 En base al siguiente javascript, usa destructuring para crear 3 variables 
 llamadas fruit1, fruit2 y fruit3, con los valores del array. Posteriormente
 imprimelo por consola. */
 
 const fruits = ['Banana', 'Strawberry', 'Orange'];
+
+let [fruit1, fruit2, fruit3] = fruits;
+console.log(fruit1);
+console.log(fruit2);
+console.log(fruit3);
 
 /* 2.3 En base al siguiente javascript, usa destructuring para crear 2 
 variables igualandolo a la función e imprimiendolo por consola. */
@@ -40,6 +58,9 @@ const animalFunction = () => {
     return {name: 'Bengal Tiger', race: 'Tiger'}
 };
 
+let [nombre, raza] = animalFunction();
+console.log(nombre);
+console.log(raza);
 
 /* 2.4 En base al siguiente javascript, usa destructuring para crear las 
 variables name y itv con sus respectivos valores. Posteriormente crea 
@@ -48,6 +69,14 @@ y comprueba que todo esta bien imprimiendolo. */
 
 const car = {name: 'Mazda 6', itv: [2015, 2011, 2020] }
 
+let {name, itv} = car;
+console.log(name);
+console.log(itv);
+
+let [year1, year2, year3] = itv;
+console.log(year1);
+console.log(year2);
+console.log(year3);
 
 // -----------------------------------------------------------------
 // ** Iteración #3: Spread Operator **
@@ -56,12 +85,20 @@ const car = {name: 'Mazda 6', itv: [2015, 2011, 2020] }
 // 3.1 Dado el siguiente array, crea una copia usando spread operators.
 const pointsList = [32, 54, 21, 64, 75, 43]
 
+const pointsListCopy = [...pointsList];
+console.log(pointsListCopy);
+
 // 3.2 Dado el siguiente objeto, crea una copia usando spread operators.
 const toy = {name: 'Bus laiyiar', date: '20-30-1995', color: 'multicolor'};
+
+const toyCopy = {...toy};
+console.log(toyCopy);
 
 //3.3 Dado los siguientes arrays, crea un nuevo array juntandolos usando spread operatos.
 const pointsList = [32, 54, 21, 64, 75, 43];
 const pointsLis2 = [54,87,99,65,32];
+
+const pointListsJoined = {...pointsList, ...pointsLis2};
 
 // 3.4 Dado los siguientes objetos. Crea un nuevo objeto fusionando los dos con spread operators.
 const toy = {name: 'Bus laiyiar', date: '20-30-1995', color: 'multicolor'};
@@ -70,6 +107,9 @@ const toyUpdate = {lights: 'rgb', power: ['Volar like a dragon', 'MoonWalk']}
 /* 3.5 Dado el siguiente array. Crear una copia de él eliminando la posición 2 
 pero sin editar el array inicial. De nuevo, usando spread operatos. */
 const colors = ['rojo', 'azul', 'amarillo', 'verde', 'naranja'];
+
+const toysTogether = {...toy, ...toyUpdate};
+console.log(toysTogether);
 
 // -----------------------------------------------------------------
 // ** Iteración #4: Map **
