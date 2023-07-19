1280f0efc46fe3df054f46ba885cb976701f939b
Emetepe <martape@gmail.com>
2023-07-19 21:19:50 +0200

    DOM

--- JAVASCRIPT/neoland-js-DOM/app.js
+++ JAVASCRIPT/neoland-js-DOM/app.js
@@ -50,9 +50,64 @@ console.log(classPokSelect);
 
 
 // 1.5 Usa querySelector para mostrar por consola todos los elementos con el atributo data-function="testMe".
-const atrSelect = document.querySelector("#testMe");
+const atrSelect = document.querySelectorAll('span[data-function="testMe"]');
 console.log(atrSelect);
 
 // 1.6 Usa querySelector para mostrar por consola el 3 personaje con el atributo data-function="testMe".
 const atr3Select = document.querySelector("#3 #testMe");
 console.log(atr3Select);
+
+// Iteración #2 Modificando el DOM //
+
+/* Dado el siguiente HTML 
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+	<h2 class="fn-insert-here"></h2>
+	<p class="fn-remove-me">Eliminame!</p>
+  <p>No me elimines!</p>
+  <p>No me elimines!</p>
+  <p class="fn-remove-me">Eliminame!</p>
+  <p>No me elimines!</p>
+  <p class="fn-remove-me">Eliminame!</p>
+  <p class="fn-remove-me">Eliminame!</p>
+  <p>No me elimines!</p>
+	<div></div><div></div>
+	<div class="fn-insert-here"></div>
+  <div class="fn-insert-here"></div>
+</body>
+</html> 
+*/
+
+// 2.1 Inserta dinámicamente en eun html un div vacío con javascript
+
+
+// 2.2 Inserta dinámicamente en eun html un div que contenga una p con javascript
+
+
+// 2.3 Inserta dinámicamente en eun html un div que contenga 6 p utilizando un loop con javascript
+
+
+// 2.4 Inserta dinámicamente con javascript en un html una p con el texto "Soy dinámico!"
+
+
+// 2.5 Inserta en el h2 con la clase .f-insert-here el texto "Wubba Lubba dub dub"
+
+
+// 2.6. Basándote en el siguiente array crea una lista ul > li con los textos del array 
+const apps = ["Facebook", "Netflix", "Instagram", "Snapchat", "Twitter"];
+
+
+// 2.7 Elimina todos los nodos que tengan la clase .fn-remove-me
+
+
+// 2.8 Inserta una p con el texto "Voy en medio!" entre los dos div. Recuerda que solo puedes insertar elementos con .appendChild
+
+
+// 2.9 Inserta p con el texto "Voy dentro!", dentro de todos los div con la clase .fn-insert-here
