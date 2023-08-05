09fcd9fa32c6df8e8c5e78279cddccbdfb7c0c7a
Emetepe <martape@gmail.com>
2023-08-05 11:18:22 +0200

    Users

---
+++ NODE/1. FicherosAndWebScrapping/6.-Users/utils/randomCode.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+// archivo en utils -> randomCode.js
+const randomCode = () => {
+    let code = Math.floor(Math.random() * (999999 - 100000) + 100000);
+    return code;
+  };
+  
+  module.exports = randomCode;
